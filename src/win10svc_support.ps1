9a1450b4920b89a35ef01930f059e8cd1f5c0b74
Ikem Krueger <ikem.krueger@gmail.com>
2022-05-03 17:36:20 +0200

    Add dummy functions

---
+++ src/win10svc_support.ps1
@@ -0,0 +1,23 @@
+function Show-About-Dialog() {
+	Write-Host 'Debug: Show-About-Dialog()'
+}
+
+function Show-Log-File() {
+	Write-Host 'Debug: Show-Log-File()'
+}
+
+function Update-Progress-Bar() {
+	Write-Host 'Debug: Update-Progress-Bar()'
+}
+
+function Repair-Windows-Services() {
+	Write-Host 'Debug: Repair-Windows-Services()'
+}
+
+function Init() {
+	Write-Host 'Debug: Init()'
+}
+
+function Destroy-Window() {
+	Write-Host 'Debug: Destroy-Window()'
+}
