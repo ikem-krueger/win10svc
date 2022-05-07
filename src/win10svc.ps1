f180ad74dba299ce47070e20c49d01091e1b1590
Ikem Krueger <ikem.krueger@gmail.com>
2022-05-07 16:23:43 +0200

    Remove UseVisualStyleBackColor

--- src/win10svc.ps1
+++ src/win10svc.ps1
@@ -49,19 +49,16 @@ $TButton1 = New-Object System.Windows.Forms.Button
 $TButton1.Location = "12,168"
 $TButton1.Size = "75,25"
 $TButton1.Text = "&About"
-$TButton1.UseVisualStyleBackColor = $true
 
 $TButton2 = New-Object System.Windows.Forms.Button
 $TButton2.Location = "102,168"
 $TButton2.Size = "75,25"
 $TButton2.Text = "&Log"
-$TButton2.UseVisualStyleBackColor = $true
 
 $TButton3 = New-Object System.Windows.Forms.Button
 $TButton3.Location = "195,168"
 $TButton3.Size = "75,25"
 $TButton3.Text = "&Repair"
-$TButton3.UseVisualStyleBackColor = $true
 
 $top = New-Object System.Windows.Forms.Form
 
